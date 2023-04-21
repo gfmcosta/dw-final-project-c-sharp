fe4999d2166787a4a71afbd47f43bb685ac1b487
Goncalo Costa <gfmcosta03@gmail.com>
2023-04-21 14:58:40 +0100

    Initial commit

---
+++ DW-Final-Project/DW-Final-Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DW_Final_Project.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
